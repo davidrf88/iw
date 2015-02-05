fe42407c4da8e953d287f222808ceb3a18724476
davidrf88 <davidrf88@gmail.com>
2015-02-05 01:59:46 -0700

    k

--- www/js/index.js
+++ www/js/index.js
@@ -1,64 +1,16 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
 
 function photo()
 {
-alert("entro");
+
 navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
     destinationType: Camera.DestinationType.DATA_URL
 });
 
 function onSuccess(imageData) {
-   alert(imageData);
+   
   var image = document.getElementById('CapturedImg');
     image.src = "data:image/jpeg;base64," + imageData;
    
@@ -73,9 +25,44 @@ function onSuccess(imageData) {
 }
 
 function onFail(message) {
-    alert('Failed because: ' + message);
+   // alert('Failed because: ' + message);
 }
 
 
+
+}
+function sharing()
+{
+  alert('entro');
+  window.plugins.socialsharing.share('Message only');
+alert("salio");
+
+}
+
+function Contacts()
+{
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+   var ulist = $("#listCon")
+     for (var i=0; i<contacts.length; i++) 
+    {
+
+ulist.append("<li>"+contacts[i].name.givenName+"</li>");
+      
+    }
+
+};
+
+function onError(contactError) {
+    alert('onError!');
+};
+
+// find all contacts with 'Bob' in any name field
+var options      = new ContactFindOptions();
+options.filter   = "";
+options.multiple = true;
+options.desiredFields = ["name"];
+var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+navigator.contacts.find(fields, onSuccess, onError, options);
 
 }
